c9ed7b8894c1f6cc46b71f2e471dd78fb59e30d5
xfaustnwe3089490384983894 <evilfaust88@gmail.com>
2025-07-23 19:29:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
+import express from 'express';
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
